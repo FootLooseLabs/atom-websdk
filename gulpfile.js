a9fc9c0905163d52bd11c262585c2113e74bd3ac
Apratim <apratim.ankur@gmail.com>
2025-11-02 20:13:40 +0530

    gulpfil buildmin for opt iife build

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,80 @@
-const path = require('path');
+const path = require("path");
 
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var concatCss = require('gulp-concat-css');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var uglifyCss = require('gulp-uglifycss');
-var htmlmin = require('gulp-htmlmin');
-var inlinesource = require('gulp-inline-source');
-var run = require('gulp-run-command').default;
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var concatCss = require("gulp-concat-css");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var uglifyCss = require("gulp-uglifycss");
+var htmlmin = require("gulp-htmlmin");
+var inlinesource = require("gulp-inline-source");
+var run = require("gulp-run-command").default;
 var del = require("del");
-const headerComment = require('gulp-header-comment');
+const headerComment = require("gulp-header-comment");
 
-var srcDir = "src/"
-var distDir = "dist/"
+var srcDir = "src/";
+var distDir = "dist/";
 
 //VARIABLES
 var cmpDistTmpFile = "build/sdk.js";
-var jsFiles = ['./lib/*.js', '../node_modules/muffin/dist/muffin.min.js'];
-var jsDest = '/';
+var jsFiles = ["./lib/*.js", "../node_modules/muffin/dist/muffin.min.js"];
+var jsDest = "/";
 
-var htmlFiles = ['index.src.html'];
-var htmlDest = '.';
-
+var htmlFiles = ["index.src.html"];
+var htmlDest = ".";
 
 var getSrcDirs = (_dirs) => {
-    return _dirs.map((_dir) => {
-        return path.join(srcDir, _dir);
-    });
-}
+  return _dirs.map((_dir) => {
+    return path.join(srcDir, _dir);
+  });
+};
 
 var getDestDir = (_dir) => {
-    return path.join(distDir, _dir);
-}
+  return path.join(distDir, _dir);
+};
 
+gulp.task("buildes6js", run("./node_modules/.bin/rollup -c"));
 
-gulp.task('buildes6js', run("./node_modules/.bin/rollup -c"));
-
-
-gulp.task('buildJs', function () {
-    return gulp.src([].concat(getSrcDirs(jsFiles), cmpDistTmpFile))
-        .pipe(concat('sdk.js'))
-        // .pipe(gulp.dest(jsDest))
-        .pipe(rename('sdk.min.js'))
-        // .pipe(uglify())
-        .pipe(headerComment(`
+gulp.task("buildJs", function () {
+  return gulp
+    .src([].concat(getSrcDirs(jsFiles), cmpDistTmpFile))
+    .pipe(concat("sdk.js"))
+    .pipe(
+      headerComment(`
     License: <%= pkg.license %>
     Generated on <%= moment().format('YYYY') %>
     Author: <%= _.capitalize(pkg.author) %>
     Version: <%= pkg.version %>
-  `))
-        .pipe(gulp.dest(getDestDir(jsDest)));
+  `),
+    )
+    .pipe(gulp.dest(getDestDir(jsDest)))
+    .pipe(rename("sdk.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest(getDestDir(jsDest)));
 });
 
-gulp.task('buildHtml', () => {
-    var options = {
-        compress: false,
-        attribute: 'inline-src'
-    };
-    return gulp.src(getSrcDirs(htmlFiles))
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(rename('index.html'))
-        .pipe(gulp.dest(getDestDir(htmlDest)))
-        .pipe(inlinesource(options))
-        .pipe(gulp.dest(getDestDir(htmlDest)));
+gulp.task("buildHtml", () => {
+  var options = {
+    compress: false,
+    attribute: "inline-src",
+  };
+  return gulp
+    .src(getSrcDirs(htmlFiles))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(rename("index.html"))
+    .pipe(gulp.dest(getDestDir(htmlDest)))
+    .pipe(inlinesource(options))
+    .pipe(gulp.dest(getDestDir(htmlDest)));
 });
 
 sanitise = () => {
-    del('_cmps_tmp', {force: true})
-}
+  del("_cmps_tmp", { force: true });
+};
 
-gulp.task('buildAll', gulp.series('buildes6js', 'buildJs', function done(done) {
+gulp.task(
+  "buildAll",
+  gulp.series("buildes6js", "buildJs", function done(done) {
     sanitise();
     done();
-}));
+  }),
+);
